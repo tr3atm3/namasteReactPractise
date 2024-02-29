0c5edc684e4c9fe9172050db03e50e24b448a315
tr3atm3 <tttushar.789@gmail.com>
2024-02-29 19:31:13 +0530

    routeAdded

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<RouterProvider router={appRouter} />);
